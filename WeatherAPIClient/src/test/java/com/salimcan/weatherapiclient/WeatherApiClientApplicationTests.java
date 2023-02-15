2b5c31cb2adee1fe02a8040aac0e078a42885e6e
SalimcanK <salimcankaya97@gmail.com>
2023-02-15 15:25:03 +0300

    init

---
+++ WeatherAPIClient/src/test/java/com/salimcan/weatherapiclient/WeatherApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salimcan.weatherapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
